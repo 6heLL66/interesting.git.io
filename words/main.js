cce90498443c78a0a95bf780005299d39a72fe9e
6heLL66 <51162582+6heLL66@users.noreply.github.com>
2020-03-05 22:06:21 +0300

    Update main.js

--- words/main.js
+++ words/main.js
@@ -6,71 +6,96 @@ var pixels = [];
 var myImageData = ctx.createImageData(canvas.width, canvas.height);
 var canvasTest = document.getElementById('canvaTest');
 var ctxTest = canvasTest.getContext("2d");
-ctxTest.fillStyle = "#000";
-ctxTest.font = "100pt Arial";
-ctxTest.fillText("HeLLo", 0, 100);
-var text = ctxTest.measureText("HeLLo");
-canvasTest.width = text.width;
-ctxTest.fillStyle = "#000";
-ctxTest.font = "100pt Arial";
-ctxTest.fillText("HeLLo", 0, 100);
-var wordPixels = ctxTest.getImageData(0,0,canvasTest.width,canvasTest.height);
-var to = [];
-var time = 0;
+var c = 0;
+var animated = false;
 
-for(let i = 0;i < wordPixels.data.length;i++){
-	if(wordPixels.data[i] == 255){
-		let y = Math.round((i-3)/4/canvasTest.width);
-		let x = ((i-3)/4)%canvasTest.width;
-		to.push([x,y]);
-	}
-}
 
-function setPixels(n){
-	for(let i = 0;i<n;i++){
-		var posX = Math.round(Math.random()*(canvas.width));
-		var posY = Math.round(Math.random()*(canvas.height));
-		var speed = [];
-		speed[0] = (to[i][0] - posX)/5;
-		speed[1] = (to[i][1] - posY)/5;
-		var obj = {
-			"color" : [0,0,0,255],
-			"pos" : [posX,posY],
-			"to" : [to[i][0],to[i][1]],
-			"speed" : speed
+
+document.getElementById('btn').onclick = function(){
+	if(animated)return 0;
+	c++;
+	if(c>1){
+		for(let i = 0;i<pixels.length;i++){
+			let x = Math.round(Math.random()*canvas.width);
+			let y =  Math.round(Math.random()*canvas.height);
+			pixels[i].to[0] = x;
+			pixels[i].to[1] = y;
+			pixels[i].speed[0] = (x - pixels[i].pos[0]);
+			pixels[i].speed[1] = (y - pixels[i].pos[1]);
+			var time = 0;
+			up = setInterval(update,20);
+			return 0;
 		}
-		pixels.push(obj);
 	}
-}
-
-function drawPixels(){
-	canvas.width = canvas.width;
-	myImageData = ctx.createImageData(canvas.width, canvas.height);
-	
-	for(let i = 0;i<pixels.length;i++){
-		let color = pixels[i].color;    
-		let x = Math.round(pixels[i].pos[0]),
-			y = Math.round(pixels[i].pos[1]);
-		myImageData.data[ (y*canvas.width*4)+ ((x)*4 )] = color[0];
-		myImageData.data[ (y*canvas.width*4)+ ((x)*4 ) +1] = color[1];
-		myImageData.data[ (y*canvas.width*4)+ ((x)*4 ) +2] = color[2];
-		myImageData.data[ (y*canvas.width*4)+ ((x)*4 ) +3] = color[3];
+	ctxTest.fillStyle = "#000";
+	ctxTest.font = "100pt Arial";
+	ctxTest.fillText(document.getElementById('in').value, 0, 100);
+	var text = ctxTest.measureText(document.getElementById('in').value);
+	canvasTest.width = text.width;
+	ctxTest.fillStyle = "#000";
+	ctxTest.font = "100pt Arial";
+	ctxTest.fillText(document.getElementById('in').value, 0, 100);
+	var wordPixels = ctxTest.getImageData(0,0,canvasTest.width,canvasTest.height);
+	var to = [];
+	var time = 0;
+	for(let i = 3;i < wordPixels.data.length;i+=4){
+		if(wordPixels.data[i] == 255){
+			let y = Math.round(i/4/canvasTest.width);
+			let x = (i/4)%canvasTest.width;
+			to.push([x,y]);
+		}
 	}
-	ctx.putImageData(myImageData, 0, 0);			
-}
-
-function update(){
-	time += 20;
-	if(time == 5000){
-		time = 0;
-		clearInterval(up);
-	}
-	for(let i = 0;i<pixels.length;i++){
-		pixels[i].pos[0] += pixels[i].speed[0]/50;
-		pixels[i].pos[1] += pixels[i].speed[1]/50;
-	}
+	setPixels(to.length);
 	drawPixels();
+	var up = setInterval(update,20);
+
+	function setPixels(n){
+		for(let i = 0;i<n;i++){
+			var posX = Math.round(Math.random()*(canvas.width));
+			var posY = Math.round(Math.random()*(canvas.height));
+			var speed = [];
+			speed[0] = (to[i][0] - posX)/5;
+			speed[1] = (to[i][1] - posY)/5;
+			var obj = {
+				"color" : [0,0,0,255],
+				"pos" : [posX,posY],
+				"to" : [to[i][0],to[i][1]],
+				"speed" : speed
+			}
+			pixels.push(obj);
+		}
+	}
+
+	function drawPixels(){
+		canvas.width = canvas.width;
+		myImageData = ctx.createImageData(canvas.width, canvas.height);
+		
+		for(let i = 0;i<pixels.length;i++){
+			let color = pixels[i].color;    
+			let x = Math.round(pixels[i].pos[0]),
+				y = Math.round(pixels[i].pos[1]);
+			myImageData.data[ (y*canvas.width*4)+ ((x)*4 )] = color[0];
+			myImageData.data[ (y*canvas.width*4)+ ((x)*4 ) +1] = color[1];
+			myImageData.data[ (y*canvas.width*4)+ ((x)*4 ) +2] = color[2];
+			myImageData.data[ (y*canvas.width*4)+ ((x)*4 ) +3] = color[3];
+		}
+		ctx.putImageData(myImageData, 0, 0);			
+	}
+
+	function update(){
+		time += 20;
+		console.log(time);
+		animated = true;
+		if(time == 5000){
+			time = 0;
+			clearInterval(up);
+			animated = false;
+		}
+		for(let i = 0;i<pixels.length;i++){
+			pixels[i].pos[0] += pixels[i].speed[0]/50;
+			pixels[i].pos[1] += pixels[i].speed[1]/50;
+		}
+		drawPixels();
+	}
 }
-setPixels(to.length);
-drawPixels();
-var up = setInterval(update,20);
+
